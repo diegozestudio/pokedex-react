55f998c274261ecf3d439491561e50a2a907756f
Diego Zamora <diegoz.estudio@gmail.com>
2022-02-14 20:45:56 -0300

    comment redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,10 @@ import thunk from "redux-thunk";
 
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  // compose(
+  applyMiddleware(thunk)
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // )
 );
 
 export default store;
