8b309258d1ba30baf84e6ec6fb39f4d845563931
Diego Zamora <diegoz.estudio@gmail.com>
2022-04-23 06:10:09 -0300

    try fix button style

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,34 +1,34 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import "./pokedex.css";
-import PokedexScreen from "./PokedexScreen";
-import PokemonForm from "./PokemonForm";
+import React from 'react'
+import { useSelector } from 'react-redux'
+import './pokedex.css'
+import PokedexScreen from './PokedexScreen'
+import PokemonForm from './PokemonForm'
 
 function Pokedex() {
-  const loading = useSelector((state) => state.loading);
+  const loading = useSelector((state) => state.loading)
   return (
-    <div className="pokedex">
-      <div className="pokedex-body">
-        <div className="pokedex-body-top">
-          <div className={`light is-sky is-big ${loading && "is-animated"}`} />
-          <div className="light is-red" />
-          <div className="light is-yellow" />
-          <div className="light is-green" />
+    <div className='pokedex'>
+      <div className='pokedex-body'>
+        <div className='pokedex-body-top'>
+          <div className={`light is-sky is-big ${loading && 'is-animated'}`} />
+          <div className='light is-red' />
+          <div className='light is-yellow' />
+          <div className='light is-green' />
         </div>
-        <div className="pokedex-screen-container">
+        <div className='pokedex-screen-container'>
           <PokedexScreen />
         </div>
-        <div className="pokedex-body-bottom">
-          <div className="pokedex-body-bottom-lights">
-            <div className="light is-blue is-medium" />
-            <div className="light is-green is-large" />
-            <div className="light is-orange is-large" />
+        <div className='pokedex-body-bottom'>
+          <div className='pokedex-body-bottom-lights'>
+            <div className='light is-blue is-medium' />
+            <div className='light is-green is-large' />
+            <div className='light is-orange is-large' />
           </div>
           <PokemonForm />
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Pokedex;
+export default Pokedex
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,36 +1,36 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { getPokemon } from "../actions";
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { getPokemon } from '../actions'
 
 function PokemonForm() {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('')
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    if (input !== "") {
-      const pokemonID = input.toLowerCase();
-      dispatch(getPokemon(pokemonID));
-      setInput("");
-      return;
+    e.preventDefault()
+    if (input !== '') {
+      const pokemonID = input.toLowerCase()
+      dispatch(getPokemon(pokemonID))
+      setInput('')
+      return
     }
-  };
+  }
 
   return (
-    <form className="pokemon-form" onSubmit={handleSubmit}>
+    <form className='pokemon-form' onSubmit={handleSubmit}>
       <input
-        className="pokemon-input"
-        type="text"
-        name="pokemon"
+        className='pokemon-input'
+        type='text'
+        name='pokemon'
         value={input}
-        placeholder="Busca tu pokemon"
+        placeholder='Busca tu pokemon'
         onChange={(e) => setInput(e.target.value)}
-        autoComplete="off"
+        autoComplete='off'
       />
-      <input type="submit" className="pokemon-btn" value="" />
+      <input type='submit' className='pokemon-btn' value='' />
     </form>
-  );
+  )
 }
 
-export default PokemonForm;
+export default PokemonForm
